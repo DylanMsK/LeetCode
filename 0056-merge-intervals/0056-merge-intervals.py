b12508a549e5f1057f593196cee4afd9a52209b7
Kang Minsu <kms920612@gmail.com>
2025-02-05 09:33:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+
+        merged = [intervals.pop(0)]
+        while intervals:
+            start, end = intervals.pop(0)
+            for i in range(len(merged)):
+                s, e = merged[i]
+                if start <= e:
+                    merged[i][1] = end
+                    break
+            else:
+                merged.append([start, end])
+
+        return merged
