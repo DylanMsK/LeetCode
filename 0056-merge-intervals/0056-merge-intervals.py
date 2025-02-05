e792f8e6f6e2e0785b5938e1e3cb42f805ccf45b
Kang Minsu <kms920612@gmail.com>
2025-02-05 09:39:43 +0900

    Time: 1870 ms (5.82%), Space: 21.7 MB (24.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,9 +3,8 @@ class Solution:
         intervals.sort(key=lambda x: x[0])
 
         merged = [intervals.pop(0)]
-        while intervals:
-            start, end = intervals.pop(0)
-            for i in range(len(merged)):
+        for start, end in intervals:
+            for i in range(len(merged)-1, -1, -1):
                 s, e = merged[i]
                 if start <= e:
                     merged[i][1] = max(end, e)
