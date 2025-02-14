2ffc946da16f763c6303023590fcfb35318b5677
Kang Minsu <kms920612@gmail.com>
2025-02-14 13:28:09 +0900

    Time: 0 ms (100%), Space: 17.8 MB (38.53%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        length = len(str_x)
+        for i in range(length // 2):
+            if str_x[i] != str_x[length-i-1]:
+                return False
+        return True
